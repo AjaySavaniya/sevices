c6ae6d740d203afcff13e02361538dd1796cf65e
Ajay Savaniya <123919733+AjaySavaniya@users.noreply.github.com>
2023-10-08 20:27:46 +0530

    Add files via upload

---
+++ _next/static/emzH0m7bU8Fp0C1ztUrrO/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = function(s) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": ["static/css/642115d9cc92423f.css", "static/chunks/pages/index-0ae2a45be1974a34.js"],
+    
+       "/services": ["static/css/88a736bb7da5e694.css", "static/chunks/pages/services-616accdef6f00828.js"],
+
+        sortedPages: [ "/services"]
+    }
+}("static/chunks/683-351ff7c424301786.js"), self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
---
+++ _next/static/emzH0m7bU8Fp0C1ztUrrO/_ssgManifest.js
@@ -0,0 +1,2 @@
+self.__SSG_MANIFEST = new Set(["\u002F", "\u002Fabout", "\u002Fblog\u002F[id]", "\u002Ffriends", "\u002Fwork", "\u002Fwork\u002F[id]"]);
+self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
